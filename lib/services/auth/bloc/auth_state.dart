039e59619f56ccdaa3ebeeaafabe2451af5a2d25
Amir55143 <aamirnoor55143@gmail.com>
2023-05-30 16:55:42 +0500

    Bloc Auth

---
+++ lib/services/auth/bloc/auth_state.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'auth_user.dart';
+
+@immutable
+abstract class AuthState {
+  const AuthState() ;
+}
+
+class AuthStateLoading extends AuthState {
+  const AuthStateLoading();
+}
+
+class AuthStateLoggedIn extends AuthState {
+  final AuthUser user;
+  const AuthStateLoggedIn(this.user);
+}
+
+class AuthStateLogInFailure extends AuthState {
+  final Exception exception;
+  const AuthStateLogInFailure(this.exception);
+}
+
+class AuthStateNeedVarification extends AuthState {
+  const AuthStateNeedVarification();
+}
+
+class AuthStateLoggedOut extends AuthState {
+  const AuthStateLoggedOut();
+}
